f1422d1e9f6f5ba5f022aaf76c993878fd4ab252
dariomnalerio <5725@holbertonstudents.com>
2022-09-27 07:24:11 -0700

    added 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle)
+/**
+ *  * main - Prints "Programming is like building a multilingual
+ *   *                puzzle, followed by a new line.
+ *    *
+ *     * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle);
+	return(0);
+}
