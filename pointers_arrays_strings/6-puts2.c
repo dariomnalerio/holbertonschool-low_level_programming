0860dff46cddc835c0a114da29a7eaae73766612
dariomnalerio <5725@holbertonstudents.com>
2022-10-07 06:32:40 -0700

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string
+ * @str: String to be printed
+*/
+
+void puts2(char *str)
+{
+	int n, len = 0;
+
+	while (str[len++])
+		len++;
+
+	for (n = 0 ; n <= len ; n += 2)
+		_putchar(str[n]);
+
+	_putchar('\n');
+}
