80321633046e4d2977ec5d7e7308f123a38b3cae
dariomnalerio <5725@holbertonstudents.com>
2022-10-10 14:20:44 -0700

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+*/
+
+void puts_half(char *str)
+{
+	int n, len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	if ((len % 2) == 0)
+		n = len / 2;
+	else
+		n = (len - 1) / 2;
+
+	for (n = len / 2 ; n < len ; n++)
+		_putchar(str[n]);
+
+	_putchar('\n');
+}
