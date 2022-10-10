a5a0fd4ae8961112a3800e9bf6ef1e31a1c02802
dariomnalerio <5725@holbertonstudents.com>
2022-10-10 14:29:02 -0700

    fix

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -16,9 +16,9 @@ void puts_half(char *str)
 	if ((len % 2) == 0)
 		n = len / 2;
 	else
-		n = (len - 1) / 2;
+		n = (len + 1) / 2;
 
-	for (; n < len && n < 445 ; n++)
+	for (; n < len ; n++)
 		_putchar(str[n]);
 
 	_putchar('\n');
