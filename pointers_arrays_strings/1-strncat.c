9af2c3646937316706142e5bf8758aa052f6290e
dariomnalerio <5725@holbertonstudents.com>
2022-10-11 06:31:50 -0700

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - Appends n bytes of one string to another
+ * @dest: One of the string to be appended
+ * @src: The other string to be appended
+ * @n: Number of bytes from string to be appended
+ * Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dlen = 0;
+	int i;
+
+	while (dest[dlen] != '\0')
+		dlen++;
+
+	for (i = 0 ; src[i] < src[n] ; i++)
+		dest[dlen + i] = src[i];
+
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -26,4 +26,6 @@ char *_strcpy(char *dest, char *src);
 
 char *_strcat(char *dest, char *src);
 
+char *_strncat(char *dest, char *src, int n);
+
 #endif
