548de730268e436c1166c632cb2a9b219405950d
dariomnalerio <5725@holbertonstudents.com>
2022-10-12 06:08:25 -0700

    fix

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,6 +11,9 @@ char *cap_string(char *str)
 	int i, j;
 	char check[13] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
 
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] =  (str[0] - 32)
+
 	for (i = 0 ; str[i] != '\0' ; i++)
 	{
 		for (j = 0 ; j <= 12 ; j++)
