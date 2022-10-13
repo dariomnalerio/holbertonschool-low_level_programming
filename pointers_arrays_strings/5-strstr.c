71863a2462f05fdd9e00d29660a111287ff9f3e1
dariomnalerio <5725@holbertonstudents.com>
2022-10-13 10:11:10 -0700

    fix

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * _strstr - Locates a substring
+ * @haystack: First string
+ * @needle: Substring
+ * Return: Pointer to substring or NULL
 */
 
 char *_strstr(char *haystack, char *needle)
@@ -13,7 +13,10 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0 ; haystack[i] != '\0' ; i++)
 	{
-		if ( haystack[i] == needle[0])
+		if (needle[0] == '\0')
+		       return (haystack[0]);	
+
+		else if (haystack[i] == needle[0] && haystack[i + 1] == needle[1])
 			return (&haystack[i]);
 	}
 
