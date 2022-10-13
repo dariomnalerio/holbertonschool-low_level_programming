d5f096ce0eb3caccba1e9b16da641708f5d615a9
dariomnalerio <5725@holbertonstudents.com>
2022-10-13 09:59:01 -0700

    5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	for (i = 0 ; haystack[i] != '\0' ; i++)
+	{
+		if ( haystack[i] == needle[0])
+			return (&haystack[i]);
+	}
+
+	return (NULL);
+}
