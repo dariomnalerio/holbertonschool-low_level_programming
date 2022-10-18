30aefb157566af243f04678ccc9fd6ee78409cf8
dariomnalerio <5725@holbertonstudents.com>
2022-10-18 07:18:29 -0700

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	}
+
+	printf("%d\n", i - 1);
+	(void)argv;
+	return (0);
+}
