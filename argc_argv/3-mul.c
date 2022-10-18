7cc354c3cc330bcf1467c560b28d8b54189e3434
dariomnalerio <5725@holbertonstudents.com>
2022-10-18 08:29:12 -0700

    task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	(void)argc;
+	return (0);
+}
