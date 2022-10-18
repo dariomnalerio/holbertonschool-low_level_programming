060ae34a4c78a8fcb2d41716e2ec08016ebe0655
dariomnalerio <5725@holbertonstudents.com>
2022-10-18 05:36:17 -0700

    fix

--- recursion/main.h
+++ recursion/main.h
@@ -9,5 +9,8 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int _sqrt_recursion2(int num, int i);
+int is_prime_number(int n);
+int is_prime_number2(int num, int i);
 
 #endif
