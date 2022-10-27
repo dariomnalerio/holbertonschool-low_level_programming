b845bfcaeb9f065b6afad30c03bc936e5db2e208
dariomnalerio <5725@holbertonstudents.com>
2022-10-27 05:46:22 -0700

    task 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,16 @@
-/**
- *
- *
- *
- */
+#ifndef dog_a
+#define dog_a
 
+/**
+ * struct dog - Dog struct
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+*/
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
+#endif
