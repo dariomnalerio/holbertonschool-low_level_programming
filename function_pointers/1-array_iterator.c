386f03cce36cdff8884dbe19a795f25b02f0edc9
dariomnalerio <5725@holbertonstudents.com>
2022-10-30 11:47:06 -0700

    task_2 v2

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,7 +11,12 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned long int i;
+	unsigned int i;
+
+	if (!action || !array)
+	{
+		return;
+	}
 
 	for (i = 0; i < size; i++)
 	{
