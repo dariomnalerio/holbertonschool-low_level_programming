8d7344aa2876417417af77a4c8003070d681ebce
dariomnalerio <5725@holbertonstudents.com>
2022-11-23 05:34:15 -0800

    task_0 v0.0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and prints it
+ * @letters: Number of letters to be read and printed
+ * @filename: Name of the file
+ *
+ * Return: On success: actual number of letters read and printed
+ * If it fails: -1 if buffer is NULL, 0 otherwise
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, r, w;
+	char *buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+		return (-1);
+
+	fd = open(filename, O _RDONLY);
+	if (fd == -1)
+		return (0);
+
+	r = read(fd, buffer, letters);
+	if (r == -1)
+		return (0);
+
+	w = write(STDOUT_FILENO, buffer, r);
+	if (w == -1)
+		return (0);
+
+	return (w);
+}
