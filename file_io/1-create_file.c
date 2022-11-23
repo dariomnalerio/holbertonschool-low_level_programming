94df0b62bf9f3ec63ddcb507a8d4ade782ae98ff
dariomnalerio <5725@holbertonstudents.com>
2022-11-23 07:23:02 -0800

    task_1 v0.1 typo fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	for (strlen = 0, text_content[strlen != '\0', strlen++])
+	for (strlen = 0, text_content[strlen] != '\0', strlen++])
 	{
 	}
 
