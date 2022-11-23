63dcd59eb32e40342c817227e2be30487d2c8422
dariomnalerio <5725@holbertonstudents.com>
2022-11-23 07:48:42 -0800

    task_1 v1.1 removed buffer string

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,7 +12,6 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, w, strlen;
-	char *buffer;
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
@@ -25,9 +24,7 @@ int create_file(const char *filename, char *text_content)
 	{
 	}
 
-	buffer = malloc(sizeof(char) * strlen);
-
-	w = write(fd, buffer, strlen);
+	w = write(fd, text_conect, strlen);
 	if (w == -1)
 		return (-1);
 
