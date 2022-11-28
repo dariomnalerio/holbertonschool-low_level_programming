4745862f021a88fcb91dc9d297473cb949440c4a
dariomnalerio <5725@holbertonstudents.com>
2022-11-27 17:17:49 -0800

    task_0 v1.1

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,13 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_t - Creates a hash table
+ * hash_table_create - Creates a hash table
  * @size: Size of the array of the nodes
  *
  * Return: Pointer to the created hash table
 */
-
-#include "hash_tables.h"
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
