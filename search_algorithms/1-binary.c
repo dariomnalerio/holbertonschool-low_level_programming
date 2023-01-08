ba9e5f9df95f5e53473114235feb239373bdac61
dariomnalerio <dariomnalerio@gmail.com>
2023-01-08 14:52:17 -0800

    Task 1 fix

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,8 +1,12 @@
 #include "search_algos.h"
 
 /**
- *
- *
+ * binary_search - searches for a value in a sorted array of integers
+ * using the Binary search algorithm
+ * @array: Array
+ * @size: size of the array
+ * @value: value to be found
+ * Return: value index, -1 otherwise
  */
 
 int binary_search(int *array, size_t size, int value)
@@ -14,45 +18,49 @@ int binary_search(int *array, size_t size, int value)
 }
 
 /**
- *
- *
- *
+ * recursive - binary search recursive function
+ * @array: Array
+ * @low: array's lowest value to search through
+ * @high: array's highest value search through
+ * @value: value to be found
+ * Return: value index, -1 otherwise
 */
 
-int recursive (int *array, size_t low, size_t high, int value)
+int recursive(int *array, size_t low, size_t high, int value)
 {
 	size_t mid;
 
-	if (low >= high)
+	if (low > high)
 		return (-1);
+	mid = (low + high) / 2;
+	if (value == array[mid])
+		return (mid);
+	else if (value > array[mid])
+	{
+		if (high - low > 1)
+			print(array, mid + 1, high);
+		return (recursive(array, mid + 1, high, value));
+	}
 	else
 	{
-		mid = (low + high) / 2;
-		if (value == array[mid])
-			return (mid);
-		else if (value > array[mid])
-		{
-			print(array, mid + 1, high);
-				return (recursive(array, mid + 1, high, value));
-		}
-		else
-		{
+		if (high - low > 1)
 			print(array, low, mid - 1);
-			return (recursive(array, low, mid - 1, value));
-		}
-
+		return (recursive(array, low, mid - 1, value));
 	}
 }
 
+
 /**
- *
- *
- *
+ * print - Prints array
+ * @array: array
+ * @low: array's lowest value
+ * @high: array's highest value
 */
 
 void print (int *array, size_t low, size_t high)
 {
 	size_t i;
+
 	printf("Searching in array: ");
 	for (i = low; i <= high; i++)
 	{
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -5,7 +5,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void print (int *array, size_t low, size_t high);
-int recursive (int *array, size_t low, size_t high, int value);
+void print(int *array, size_t low, size_t high);
+int recursive(int *array, size_t low, size_t high, int value);
 
 #endif
